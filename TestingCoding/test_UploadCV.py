7f585fc5b5f1d8531ecb20df0ce05b3aa75b28af
andrewharrop <andrewhar05@gmail.com>
2021-11-20 15:03:36 -0500

    Secret files

--- TestingCoding/test_UploadCV.py
+++ TestingCoding/test_UploadCV.py
@@ -7,7 +7,8 @@ from selenium.webdriver.common.keys import Keys
 import os  # to get the resume file
 import time  # to sleep
 
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
 
 # sample application links if we don't want to run get_links.py
 URL_l2 = 'https://boards.greenhouse.io/doordash/jobs/3393928?gh_jid=3393928&rx_c=corp---canada&rx_campaign=indeed144&rx_group=129152&rx_job=3393928&rx_r=none&rx_source=Indeed&rx_ts=20211120T140008Z&rx_p=FGY5TRVQOV&rx_viewer=704eae0e2d0a11ec85ec9fccc13cdfd51924a10d0f42408aa10bf7cd9b4a6e67'
@@ -19,34 +20,36 @@ URL_l9 = 'https://jobs.lever.co/bolt/e73884bc-f2c1-4069-b496-2b9564390520'
 URL_g1 = 'https://jobs.lever.co/bolt/b286df9b-0df5-45af-8080-7b59e22d7171'
 URL_g2 = 'https://jobs.lever.co/bolt/04d798cf-65eb-49b6-b526-329e00c98b9c'
 
-URLS = [URL_l2,URL_l3 ,URL_l4 ,URL_l6 ,URL_l9, URL_g1, URL_g2 ]
+URLS = [URL_l2, URL_l3, URL_l4, URL_l6, URL_l9, URL_g1, URL_g2]
 
 
 def greenhouse(driver):
-    try:
-        print("found the attach")
-        #os.curdir()
-        time.sleep(5)
-        # driver.find_elements_by_id("resume").send_keys(os.path.join(__location__,"Michael Caneff_Resume_V3.pdf"))
-        # driver.find_elements_by_id("resume").send_keys("C:\Users\MCane\OneDrive\Documents\Personal Projects\Job Automation\Job-Process-Automation\Michael_Caneff_Resume_V3.pdf")
-        
-        
-    except NoSuchElementException:
-        pass
+    print("found the attach")
+    # os.curdir()
+    time.sleep(8)
+    # send_keys(os.path.join(__location__, 'Michael_Caneff_Resume_V3.pdf'))
+
+    # Get the secret location for cover letter
+    cover_letter = driver.find_element_by_id(
+        "s3_upload_for_cover_letter").find_element_by_xpath('//*[@type="file"]')
+    cover_letter.send_keys(
+        "/Users/andrew/Desktop/Work-Code/Job-Process-Automation/TestingCoding/cover.pdf")
+    time.sleep(2)
+
+    # Get the secret location for resume
+    resume = driver.find_element_by_id(
+        "s3_upload_for_resume").find_element_by_xpath('//*[@type="file"]')
+    resume.send_keys(
+        "/Users/andrew/Desktop/Work-Code/Job-Process-Automation/TestingCoding/Michael_Caneff_Resume_V3.pdf")
+    print("sub")
+
+    time.sleep(5)
     return print("success")
 
 
 if __name__ == '__main__':
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    for url in URLS:
-        print('\n')
+    print('\n')
 
-        if 'greenhouse' in url:
-            driver.get(url)
-            try:
-                greenhouse(driver)
-            except Exception:
-                print("failed")
-                continue
-
-    driver.close()
+    driver.get("https://boards.greenhouse.io/asana/jobs/2344893")
+    greenhouse(driver)
